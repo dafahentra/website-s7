c4e329a4cc8b37faaa5c11a8ef9206bffd859a93
Dafa Hentra <63453455+dafahentra@users.noreply.github.com>
2026-01-25 17:01:39 +0700

    p

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ import ScrollToTop from "./components/ScrollToTop";
 
 function App() {
   return (
-    <Router basename="/website-s7">
+    <Router basename="/">
       <ScrollToTop />
       <div className="min-h-screen">
         <Navbar />
