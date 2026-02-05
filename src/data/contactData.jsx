4891562033a383eea2d27cfc633f34f6350f200a
Dafa Hentra <63453455+dafahentra@users.noreply.github.com>
2026-02-05 11:24:45 +0700

    p

--- src/data/contactData.jsx
+++ src/data/contactData.jsx
@@ -42,7 +42,7 @@ icon: (
 
 // FormSubmit Configuration
 export const formSubmitConfig = {
-endpoint: "https://formsubmit.co/dapahentra@gmail.com ",
+endpoint: "https://formsubmit.co/c965ba286caff0dfc8011665bb1f691e  ",
 subject: "📧 Pesan Baru dari Website Sector Seven",
 captcha: false,
 template: "table"
