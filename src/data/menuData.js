4a2f4272b66f5b853e6d8274aa937e76c40a79ea
Dafa Hentra <63453455+dafahentra@users.noreply.github.com>
2026-04-16 22:04:12 +0700

    Revert "p"
    This reverts commit 8de061a9275eacbd7af78469dd220ee663a85388.

--- src/data/menuData.js
+++ src/data/menuData.js
@@ -1,155 +1,165 @@
-import Usutcha      from "../assets/Usutcha.png";
-import Sectorize    from "../assets/Sectorize.png";
-import MatchaLatte  from "../assets/MatchaLatte.png";
-import MatchaStraw  from "../assets/MatchaStraw.png";
-import DirtyMatcha  from "../assets/DirtyMatcha.png";
-import SeaSalt      from "../assets/SeaSalt.png";
-import Americano    from "../assets/Americano.png";
-import Cappucino    from "../assets/Cappucino.png";
-import Latte        from "../assets/Latte.png";
-import Abericano    from "../assets/Abericano.png";
-import Vanilla      from "../assets/Vanilla.png";
-import Hazelnuts    from "../assets/Hazelnuts.png";
+// src/data/menuData.js
+
+import Usutcha from "../assets/Usutcha.png";
+import Sectorize from "../assets/Sectorize.png";
+import MatchaLatte from "../assets/MatchaLatte.png";
+import MatchaStraw from "../assets/MatchaStraw.png";
+import DirtyMatcha from "../assets/DirtyMatcha.png";
+import SeaSalt from "../assets/SeaSalt.png";
+import Americano from "../assets/Americano.png";
+import Cappucino from "../assets/Cappucino.png";
+import Latte from "../assets/Latte.png";
+import Abericano from "../assets/Abericano.png";
+import Vanilla from "../assets/Vanilla.png";
+import Hazelnuts from "../assets/Hazelnuts.png";
 import Butterscotch from "../assets/Butterscotch.png";
-import Wizzie       from "../assets/Wizzie.png";
-import Palm         from "../assets/Palm.png";
-import Caramelted   from "../assets/Caramelted.png";
-import Choco        from "../assets/Choco.png";
-import Reveluv      from "../assets/Reveluv.png";
+import Wizzie from "../assets/Wizzie.png";
+import Palm from "../assets/Palm.png";
+import Caramelted from "../assets/Caramelted.png";
+import Choco from "../assets/Choco.png";
+import Reveluv from "../assets/Reveluv.png";
+import NotAvail from "../assets/NotAvail.png";
+import Kwasong from "../assets/Kwasong.png";
+import Cinnamon from "../assets/Cinnamon.png";
+import Apple from "../assets/Apple.png";
+import Plain from "../assets/Plain.png";
+import Double from "../assets/Double.png";
+import Blueberry from "../assets/Blueberry.png"
+
 
 export const menuItems = {
-  "Espresso Based": [
-    {
-      id: 2,
-      name: "Americano",
-      description: "An Americano, rich espresso shots. Bold and robust cup",
-      price: "21.000",
-      image: Americano,
-    },
-    {
-      id: 3,
-      name: "Abericano",
-      description: "An Americano mixed with strawberry jam. A vibrant and fruity twist on a classic",
-      price: "23.000",
-      image: Abericano,
-    },
-    {
-      id: 4,
-      name: "Latte",
-      description: "Our espresso flows through a channel of perfectly steamed milk",
-      price: "23.000",
-      image: Latte,
-    },
-    {
-      id: 5,
-      name: "Cappucino",
-      description: "Balanced engine of espresso, steamed milk, and a dense cap of velvety foam",
-      price: "23.000",
-      image: Cappucino,
-    },
-  ],
-  "Flavoured Lattes": [
-    {
-      id: 1,
-      name: "Sectorize",
-      description: "Bold espresso with creamy milk and subtle sweetness",
-      price: "23.000",
-      image: Sectorize,
-    },
-    {
-      id: 6,
-      name: "White Vanilla",
-      description: "The original Vanilla Latte, rich espresso, steamed milk, and timeless vanilla",
-      price: "25.000",
-      image: Vanilla,
-    },
-    {
-      id: 7,
-      name: "Buttery",
-      description: "A warm Butterscotch Latte, blend of rich espresso and buttery, melted butterscotch",
-      price: "25.000",
-      image: Butterscotch,
-    },
-    {
-      id: 8,
-      name: "Hazelnutz",
-      description: "A perfect Hazelnut Latte, bold espresso, creamy milk, and roasted hazelnut",
-      price: "25.000",
-      image: Hazelnuts,
-    },
-    {
-      id: 9,
-      name: "Palmer",
-      description: "A beauty of Palm Sugar, steamed milk, and marked with espresso",
-      price: "25.000",
-      image: Palm,
-    },
-    {
-      id: 10,
-      name: "Caramelted",
-      description: "A suspended balance of sweet, Salted Caramel Latte, and a sharp sea salt, all unified with smooth espresso",
-      price: "25.000",
-      image: Caramelted,
-    },
-  ],
-  "Matcha Series": [
-    {
-      id: 11,
-      name: "Pure Matcha",
-      description: "A clean charge of pure matcha with a soft, rounded finish",
-      price: "23.000",
-      image: Usutcha,
-    },
-    {
-      id: 12,
-      name: "Green Flag",
-      description: "Vibrant matcha latte with pure matcha suspended in creamy milk",
-      price: "27.000",
-      image: MatchaLatte,
-    },
-    {
-      id: 13,
-      name: "Red Flag",
-      description: "Sweet Strawberry purée rises to meet a creamy, earthy float of ceremonial matcha",
-      price: "27.000",
-      image: MatchaStraw,
-    },
-    {
-      id: 14,
-      name: "Dirty Matcha",
-      description: "A powerful, gritty fusion of Dirty Matcha and a shot of our signature espresso",
-      price: "27.000",
-      image: DirtyMatcha,
-    },
-    {
-      id: 15,
-      name: "Sea Salt Matcha",
-      description: "Pure matcha and milk, finished with a savory salted cream foam",
-      price: "27.000",
-      image: SeaSalt,
-    },
-  ],
-  "Milk Series": [
-    {
-      id: 16,
-      name: "Chocolate",
-      description: "A deep and decadent cup of chocolate and milk",
-      price: "25.000",
-      image: Choco,
-    },
-    {
-      id: 17,
-      name: "Red Velvet",
-      description: "Crimson-hued red velvet, bold by design",
-      price: "25.000",
-      image: Reveluv,
-    },
-    {
-      id: 18,
-      name: "Wizzie Berry",
-      description: "Milk and strawberry syrup blended into a strawberry milkshake",
-      price: "25.000",
-      image: Wizzie,
-    },
-  ],
+"Espresso Based": [
+{
+    id: 2,
+    name: "Americano",
+    description: "An Americano, rich espresso shots. Bold and robust cup",
+    price: "21.000",
+    image: Americano
+},
+{
+    id: 3,
+    name: "Abericano",
+    description: "An Americano mixed with strawberry jam. A vibrant and fruity twist on a classic",
+    price: "23.000",
+    image: Abericano
+},
+{
+    id: 4,
+    name: "Latte",
+    description: "Our espresso flows through a channel of perfectly steamed milk",
+    price: "23.000",
+    image: Latte
+},
+{
+    id: 5,
+    name: "Cappucino",
+    description: "Balanced engine of espresso, steamed milk, and a dense cap of velvety foam",
+    price: "23.000",
+    image: Cappucino
+}
+],
+"Flavoured Lattes": [
+{
+    id: 1,
+    name: "Sectorize",
+    description: "Bold espresso with creamy milk and subtle sweetness",
+    price: "23.000",
+    image: Sectorize
+},
+{
+    id: 6,
+    name: "White Vanilla",
+    description: "The original Vanilla Latte, rich espresso, steamed milk, and timeless vanillla",
+    price: "25.000",
+    image: Vanilla
+},
+{
+    id: 7,
+    name: "Buttery",
+    description: "A warm Butterscotch Latte, blend of rich espresso and buttery, melted butterscotch",
+    price: "25.000",
+    image: Butterscotch
+},
+{
+    id: 8,
+    name: "Hazelnutz",
+    description: "A perfect Hazelnut Latte, bold espresso, creamy milk, and roasted hazelnut",
+    price: "25.000",
+    image: Hazelnuts
+},
+{
+    id: 9,
+    name: "Palmer",
+    description: "A beauty of Palm Sugar, steamed milk, and marked with espresso",
+    price: "25.000",
+    image: Palm
+},
+{
+    id: 10,
+    name: "Caramelted",
+    description: "A suspended balance of sweet, Salted Caramel Latte, and a sharp sea salt, all unified with smooth espresso",
+    price: "25.000",
+    image: Caramelted
+}
+],
+"Matcha Series": [
+{
+    id: 11,
+    name: "Pure Matcha",
+    description: "A clean charge of pure matcha with a soft, rounded finish",
+    price: "23.000",
+    image: Usutcha
+},
+{
+    id: 12,
+    name: "Green Flag",
+    description: "Vibrant matcha latte with pure matcha suspended in creamy milk",
+    price: "27.000",
+    image: MatchaLatte
+},
+{
+    id: 13,
+    name: "Red Flag",
+    description: "Sweet Strawberry purée rises to meet a creamy, earthy float of ceremonial matcha",
+    price: "27.000",
+    image: MatchaStraw
+},
+{
+    id: 14,
+    name: "Dirty Matcha",
+    description: "A powerful, gritty fusion of Dirty Matcha and a shot of our signature espresso",
+    price: "27.000",
+    image: DirtyMatcha
+},
+{
+    id: 15,
+    name: "Sea Salt Matcha",
+    description: "Pure matcha and milk, finished with a savory salted cream foam",
+    price: "27.000",
+    image: SeaSalt
+},
+],
+"Milk Series": [
+{
+    id: 16,
+    name: "Chocolate",
+    description: "A deep and decadent cup of chocolate and  milk",
+    price: "25.000",
+    image: Choco
+},
+{
+    id: 17,
+    name: "Red Velvet",
+    description: "Crimson-hued red velvet, bold by design",
+    price: "25.000",
+    image: Reveluv
+},
+{
+    id: 18,
+    name: "Wizzie Berry",
+    description: "Milk and strawberry syrup blended into a strawberry milkshake",
+    price: "25.000",
+    image: Wizzie
+},
+]
 };
