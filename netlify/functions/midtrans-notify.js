390494a132e3ebcfcd28be45b013fd689230a8b2
Dafa Hentra <63453455+dafahentra@users.noreply.github.com>
2026-04-26 23:44:07 +0700

    Revert "p"
    This reverts commit 26149f271e03dc66a389454f98d82a713d463418.

--- netlify/functions/midtrans-notify.js
+++ netlify/functions/midtrans-notify.js
@@ -365,7 +365,6 @@ export const handler = async (event) => {
         method:  "POST",
         headers: {
           "Title":        "Order Masuk! " + formatRupiah(gross_amount),
-          "Priority":     "urgent",
           "Tags":         "bell",
           "Content-Type": "text/plain; charset=utf-8",
         },
